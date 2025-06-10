dbc3107b1de7ac992d1bada1a0f298e1e93e4258
h184906 <elias.frette@gmail.com>
2025-06-10 16:50:16 +0200

    BatMan

---
+++ AirCast/src/main/java/no/aircast/aircast/AirCastApplication.java
@@ -0,0 +1,13 @@
+package no.aircast.aircast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirCastApplication.class, args);
+    }
+
+}
---
+++ AirCast/src/test/java/no/aircast/aircast/AirCastApplicationTests.java
@@ -0,0 +1,13 @@
+package no.aircast.aircast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirCastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
